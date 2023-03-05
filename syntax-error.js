86061cf31ecb07230f40e431433ec240de6d23f6
sawonhossain34 <sawonhossain3491@gmail.com>
2023-03-05 21:44:07 +0600

    first commit syntax error

---
+++ syntax-error.js
@@ -0,0 +1,34 @@
+function isBigger(num){
+    if(num > 5){
+        return num;
+    }
+    else{
+        return 'wrong number';
+    }
+}
+// }
+
+const result = isBigger(11);
+// console.log(result);
+
+const mask ={
+    name: 'elon',
+    ag: 56,
+    // ag: 56
+    job: 'tasla'
+}
+// console.log(mask);
+
+const numbers =[12, 12, 36, 45 , 56, 88, 7 ,6, 65,];
+// const numbers =[12, 12, 36, 45 , 56, 88 7 ,6, 65,];
+// console.log(numbers);
+
+for(i = 0 ; i > 5 ; i++){
+    // const age = numbe[i;
+        const age = numbe[i];
+}
+// console.log(age);
+
+if(numbers.length > 5 && numbers[0] === 5){
+return numbers;
+}
