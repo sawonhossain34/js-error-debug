3efd477b0d38708cf12ef1412b5c5f1f31ba364b
sawonhossain34 <sawonhossain3491@gmail.com>
2023-03-05 22:03:15 +0600

    add type error

---
+++ type-error.js
@@ -0,0 +1,10 @@
+// const numbs = 56;
+// numbs.push(563);
+
+
+// let address;
+// console.log(address.home);
+
+let address = {name:'sawon',age:56, city:'jessore'}
+// console.log(address.homes.building);
+console.log(address?.homes?.building);
