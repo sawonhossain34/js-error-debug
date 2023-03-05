43d656f50bd8f238eb97f622981acde26e0cac57
sawonhossain34 <sawonhossain3491@gmail.com>
2023-03-05 22:16:38 +0600

    refarence error add

---
+++ refarence-error.js
@@ -0,0 +1,20 @@
+const x = "pori";
+const y = 'moni';
+// console.log(z);
+
+
+// console.log(name);
+// const name = 'annonto';
+// let name = 'jolil';
+
+
+// function add(num1,num2){
+//     return num1 + num2;
+// }
+
+
+console.log(add2(2,3));
+const add2 = (num4,num3) => {
+    return num3 + num4;
+}
+
